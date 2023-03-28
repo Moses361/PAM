ccb5d28613d20fedf9946b337f3db319b0978858
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-03-28 18:16:35 +0300

    restore previous database name

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,8 @@
 <?php
  
- $con = mysqli_connect ("localhost","root","","ecom_store");
+ $con = mysqli_connect ("localhost","root","","pam");
 
+ if (!$con){
+    die("Unable to connect to database");
+ }
  ?>
